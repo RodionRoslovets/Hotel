af9e412e2c08b843c8b1dc0b721b94764ac4718b
Rodion Roslovets <Rodion18570@gmail.com>
2019-08-06 18:49:44 +0300

    firs

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry:{
+        main: './src/index.js'
+      } ,
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    watch: true,
+    devtool: "source-map"
+};
